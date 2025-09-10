8124cf936c55953217005388191d5390b54cbf74
Joaquim Moraes <joaquimsp2006@gmail.com>
2025-09-10 17:14:34 -0300

    primeiro commit

---
+++ src/main/java/dev/java10x/cadastroDeNijas/CadastroDeNijasApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.cadastroDeNijas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNijasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNijasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/cadastroDeNijas/CadastroDeNijasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.cadastroDeNijas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNijasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
